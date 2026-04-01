ae2b6d347cee59831a3a72c3826dc4ec85e4ddac
durop <6711162+durop@users.noreply.github.com>
2026-04-01 14:15:32 +0200

    Add dotenv support for .env file configuration

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { collectDefaultMetrics, register, Gauge } from 'prom-client';
 import { ApiService } from './services/api.service';
